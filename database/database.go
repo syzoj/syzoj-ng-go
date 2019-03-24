78be2f887fd4d42c2a01f98a26d2d6c5cd08299b
hewenyang <hewenyang@hewenyang.cn>
2019-03-24 20:28:07 +0800

    Add ScanAll helper

--- database/database.go
+++ database/database.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"sync/atomic"
+    "reflect"
 
 	"github.com/sirupsen/logrus"
 )
@@ -83,4 +84,30 @@ func (t *DatabaseTxn) QueryContext(ctx context.Context, query string, args ...in
 
 func (t *DatabaseTxn) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
 	return t.tx.QueryRowContext(ctx, query, args...)
+}
+
+func ScanAll(r *sql.Rows, v interface{}) error {
+    val := reflect.ValueOf(v)
+    if val.Type().Kind() != reflect.Ptr {
+        panic("database: ScanAll: must be a pointer to a slice")
+    }
+    val = val.Elem()
+    if val.Type().Kind() != reflect.Slice {
+        panic("database: ScanAll: must be a pointer to a slice")
+    }
+    slice := reflect.Zero(val.Type())
+    elType := val.Type().Elem()
+    var i int
+    for r.Next() {
+        slice = reflect.Append(slice, reflect.Zero(elType))
+        err := r.Scan(slice.Index(i).Addr().Interface())
+        if err != nil {
+            return err
+        }
+    }
+    if err := r.Err(); err != nil {
+        return err
+    }
+    val.Set(slice)
+    return nil
 }
